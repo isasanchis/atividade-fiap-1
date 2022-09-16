4a582e76d4d3b58fbc91b3f131d31ff8b18a020e
Isabella Sanchis <isasanchis3@gmail.com>
2022-09-16 14:43:23 -0300

    Atividades da FIAP realizadas

---
+++ src/atividade1.ts
@@ -0,0 +1,31 @@
+console.log("Atividade 1:")
+for (let x: number = 1; x <= 10; x++) {
+    console.log("Tabuada do", x);
+    for (let y: number = 1; y <= 10; y++) {
+        console.log(x, "x", y, "=", x*y);
+    }
+    console.log(" ");
+}
+
+console.log("Atividade 2:")
+/* 
+CEP:       
+/^[0-9]{5}\-[0-9]{3}$/;
+
+EMAIL:   
+/^[a-z0-9._]+@[a-z0-9._]\.([a-z]+)?$/i;   
+
+CPF:      
+/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/;
+
+CNPJ:   
+/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}\-[0-9]{2}$/;
+
+RG:      
+/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\-[0-9a-Z]{1}$/; 
+*/
+
+console.log("Atividade 3:")
+/*
+/^[0-9]{3,4}\.[0-9]{3}\.[a-z]{3}\.[a-z]{3}$/;
+*/
